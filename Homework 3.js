9ec90d7b314978f3121742c32c9a0736a71c1a44
zavenmatsakanyan <99826106+zavenmatsakanyan@users.noreply.github.com>
2022-03-16 01:24:36 +0300

    Add files via upload

---
+++ Homework 3.js
@@ -0,0 +1,33 @@
+//1. Given an array․ Compute the length of the array. (without using .length)
+
+
+function elementsQuantity(arr){
+    let sum = 0
+    for(i = 0;i < arr.length; i++){
+        if(arr){
+            sum++
+        }else{
+            sum++
+        }
+    }alert(sum)
+}elementsQuantity([1,2,3,4,5,6])
+
+
+
+
+//2. Given an array of numbers. Print the sum of the elements in array.
+
+function elementsSum(arr){
+    let sum = 0
+    for(i = 0;i < arr.length; i++){
+        if(arr){
+            sum +=arr[i]
+        }else{
+            sum +=0
+        }
+    }return(sum)
+}elementsSum([-1, 0, 1, 2])
+
+
+
+//
